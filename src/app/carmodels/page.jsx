1bce49c93cc6bed6a7e25d764b65f9f79f0c1069
Lakshay kumar <imlakshaykumar@gmail.com>
2024-04-15 05:12:16 +0530

    added car data/page

--- src/app/carmodels/page.jsx
+++ src/app/carmodels/page.jsx
@@ -1,8 +1,71 @@
+import Image from "next/image";
+
+import { carData } from "@/data/carsData";
+// import fillstar from '../../../public/fillstar.svg'
+import emptystar from '../../../public/emptystar.svg'
+import carSVG from '../../../public/car.svg'
+
 export default function CarModel() {
     return (
-        <div className="contact-heading-div flex flex-col h-[25rem] items-start justify-center">
-            <h2 className=" text-4xl font-bold mb-1 w-full">Vehicle Models</h2>
-            <h3 className="font-semibold text-lg w-full">Home / Vehicle Models</h3>
-        </div>
+        <>
+            <div className="contact-heading-div flex flex-col h-[25rem] items-start justify-center">
+                <h2 className=" text-4xl font-bold mb-1 w-full">Vehicle Models</h2>
+                <h3 className="font-semibold text-lg w-full">Home / Vehicle Models</h3>
+            </div>
+
+            <div className="car-content-div">
+
+                <div className="content grid grid-cols-3 gap-y-[5rem] gap-x-[3rem] w-fit mx-auto mt-[5rem] mb-[10rem]">
+                    {
+                        carData?.map((item, index) => {
+                            return (
+                                <div key={ index } className="car-content bg-[#f2f2f2] shadow-xl rounded-md">
+                                    <Image src={ item.image } alt="Car Image" className="block w-full h-[15rem] object-cover" />
+                                    <div className="car-data-div py-4 px-8 capitalize">
+                                        <div className="car-data flex items-center justify-between py-4">
+                                            <div className="name_rating flex flex-col gap-2">
+                                                <p className="text-xl font-bold">{ item.name }</p>
+                                                <div className="rating flex">
+                                                    <Image src={ emptystar } alt="star" />
+                                                    <Image src={ emptystar } alt="star" />
+                                                    <Image src={ emptystar } alt="star" />
+                                                    <Image src={ emptystar } alt="star" />
+                                                    <Image src={ emptystar } alt="star" />
+                                                </div>
+                                            </div>
+                                            <div className="rent flex flex-col gap-1">
+                                                <p className="text-xl font-bold">{ item.rent }</p>
+                                                <p>per day</p>
+                                            </div>
+                                        </div>
+                                        <div className="additional-info grid grid-cols-2 gap-x-[10rem] gap-y-2 py-4 mb-5">
+                                            <div className="info flex gap-1 items-center">
+                                                <Image src={ carSVG } alt="carSVG" width={ 20 } />
+                                                <p className="w-full">{ item.mark }</p>
+                                            </div>
+                                            <div className="info flex gap-2 items-center">
+                                                <p className="w-full text-right">{ item.doors }</p>
+                                                <Image src={ carSVG } alt="carSVG" width={ 20 } />
+                                            </div>
+                                            <div className="info flex gap-2 items-center">
+                                                <Image src={ carSVG } alt="carSVG" width={ 20 } />
+                                                <p className="w-full">{ item.transmission }</p>
+                                            </div>
+                                            <div className="info flex gap-2 items-center">
+                                                <p className="w-full text-right">{ item.fuel }</p>
+                                                <Image src={ carSVG } alt="carSVG" width={ 20 } />
+                                            </div>
+                                        </div>
+                                        <div className="book-btn-div border-t border-gray-300 w-full text-center py-5">
+                                            <button className="text-xl font-semibold capitalize bg-[#ff4d30] w-full py-4 rounded-md text-white shadow-md">book ride</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </>
     )
 }
---
+++ src/data/carsData.js
@@ -0,0 +1,87 @@
+import audi from '../../public/images/cars/audi_a1_s_line.jpg'
+import vw_golf from '../../public/images/cars/vw_golf6.jpg'
+import toyota from '../../public/images/cars/toyotacamry.jpg'
+import bmw from '../../public/images/cars/bmw320.jpg'
+import benz from '../../public/images/cars/benz.jpg'
+import vw_passatc from '../../public/images/cars/vw_passatcc.jpg'
+
+export const carData = [
+    {
+        id: 1,
+        image: audi,
+        name: 'Audi A1 S-Line',
+        rent: '$45',
+        model: 'audi',
+        mark: 'A1',
+        year: '2012',
+        doors: '4/5',
+        AC: 'yes',
+        transmission: 'manual',
+        fuel: 'gasoline'
+    },
+    {
+        id: 2,
+        image: vw_golf,
+        name: 'VW Golf 6',
+        rent: '$37',
+        model: 'golf 6',
+        mark: 'volkswagen',
+        year: '2008',
+        doors: '4/5',
+        AC: 'yes',
+        transmission: 'manual',
+        fuel: 'diesel'
+    },
+    {
+        id: 3,
+        image: toyota,
+        name: 'toyota camry',
+        rent: '$30',
+        model: 'camry',
+        mark: 'toyota',
+        year: '2006',
+        doors: '4/5',
+        AC: 'yes',
+        transmission: 'automatic',
+        fuel: 'hybrid'
+    },
+    {
+        id: 4,
+        image: bmw,
+        name: 'BMW 320 Modernline',
+        rent: '$35',
+        model: '320',
+        mark: 'BMW',
+        year: '2012',
+        doors: '4/5',
+        AC: 'yes',
+        transmission: 'manual',
+        fuel: 'diesel'
+    },
+    {
+        id: 5,
+        image: benz,
+        name: 'Mercedes-Benz GLK',
+        rent: '$50',
+        model: 'Benz GLK',
+        mark: 'Mercedes',
+        year: '2006',
+        doors: '4/5',
+        AC: 'yes',
+        transmission: 'manual',
+        fuel: 'diesel'
+    },
+    {
+        id: 6,
+        image: vw_passatc,
+        name: 'VW Passat CC',
+        rent: '$25',
+        model: 'passat CC',
+        mark: 'volkswagen',
+        year: '2008',
+        doors: '4/5',
+        AC: 'yes',
+        transmission: 'automatic',
+        fuel: 'gasoline'
+    },
+]
