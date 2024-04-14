22a53c510f02de5631f47a56b4ffae65709d6532
Lakshay kumar <imlakshaykumar@gmail.com>
2024-04-15 03:08:51 +0530

    added team data/images/page

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -1,8 +1,33 @@
+import Image from "next/image";
+
+import { teamData } from "@/data/teamData";
+
 export default function Team() {
     return (
-        <div className="contact-heading-div flex flex-col h-[25rem] items-start justify-center">
-            <h2 className=" text-4xl font-bold mb-1 w-full">Out Team</h2>
-            <h3 className="font-semibold text-lg w-full">Home / Our Team</h3>
-        </div>
+        <>
+            <div className="contact-heading-div flex flex-col h-[25rem] items-start justify-center">
+                <h2 className=" text-4xl font-bold mb-1 w-full">Out Team</h2>
+                <h3 className="font-semibold text-lg w-full">Home / Our Team</h3>
+            </div>
+
+            <div className="team-content-div grid grid-cols-3 gap-12 w-fit mx-auto mt-[3rem] mb-[6rem]">
+                {
+                    teamData?.map((item, index) => {
+                        return (
+                            <>
+                                <div key={ index } className="team w-fit flex flex-col items-center justify-center shadow-xl  ">
+                                    <Image src={ item.image } alt="team member" className="bg-[#f2f2f2]" />
+                                    <div className="team-info flex flex-col items-center justify-center capitalize py-5">
+                                        <p className="text-xl font-bold">{ item.name }</p>
+                                        <p className="text-sm text-gray-500">{ item.profile }</p>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    })
+                }
+
+            </div>
+        </>
     )
 }
---
+++ src/data/teamData.js
@@ -0,0 +1,45 @@
+import teamMember1 from '../../public/images/team/1.png'
+import teamMember2 from '../../public/images/team/2.png'
+import teamMember3 from '../../public/images/team/3.png'
+import teamMember4 from '../../public/images/team/4.png'
+import teamMember5 from '../../public/images/team/5.png'
+import teamMember6 from '../../public/images/team/6.png'
+
+export const teamData = [
+    {
+        id: 1,
+        image: teamMember1,
+        name: 'luke miller',
+        profile: 'salesman'
+    },
+    {
+        id: 2,
+        image: teamMember2,
+        name: 'michael diaz',
+        profile: 'business owner'
+    },
+    {
+        id: 3,
+        image: teamMember3,
+        name: 'briana ross',
+        profile: 'photographer'
+    },
+    {
+        id: 4,
+        image: teamMember4,
+        name: 'lauren rivera',
+        profile: 'car detailist'
+    },
+    {
+        id: 5,
+        image: teamMember5,
+        name: 'martin rizz',
+        profile: 'mechanic'
+    },
+    {
+        id: 6,
+        image: teamMember6,
+        name: 'caitlyn hunt',
+        profile: 'manager'
+    },
+]
