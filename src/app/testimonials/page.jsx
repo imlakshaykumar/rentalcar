1a93ef675e973498717519204e4215645984a00f
Lakshay kumar <imlakshaykumar@gmail.com>
2024-04-15 03:43:29 +0530

    added testimonial data and page

--- src/app/testimonials/page.jsx
+++ src/app/testimonials/page.jsx
@@ -1,8 +1,45 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image"
+
+import { testimonialsData } from "@/data/testimonialsData"
+import dquote from '../../../public/dquote.svg'
+
 export default function Testimonials() {
     return (
-        <div className="contact-heading-div flex flex-col h-[25rem] items-start justify-center">
-            <h2 className=" text-4xl font-bold mb-1 w-full">Testimonials</h2>
-            <h3 className="font-semibold text-lg w-full">Home / Testimonials</h3>
-        </div>
+        <>
+            <div className="contact-heading-div flex flex-col h-[25rem] items-start justify-center">
+                <h2 className=" text-4xl font-bold mb-1 w-full">Testimonials</h2>
+                <h3 className="font-semibold text-lg w-full">Home / Testimonials</h3>
+            </div>
+            <div className="content flex flex-col gap-2 items-center justify-center text-center w-[750px] mx-auto">
+                <p className="text-lg font-bold">Reviewed by People</p>
+                <h2 className="text-5xl font-bold mb-5">Client's Testimonials</h2>
+                <p className="text-gray-500">Discover the positive impact we've made on the our clients by reading through their testimonials. Our clients have experienced our service and results, and they're eager to share their positive experiences with you.</p>
+            </div>
+            <div className="testimonial-content-div grid grid-cols-2 gap-[3rem] max-w-[80%] w-fit mx-auto mt-[6rem] mb-[10rem] ">
+                {
+                    testimonialsData?.map((item, index) => {
+                        return (
+                            <>
+                                <div key={ index } className="testimonial bg-gray-100 p-[3rem] rounded-md shadow-2xl">
+                                    <p className="text-2xl mb-8">"{ item.review }"</p>
+                                    <div className="name-info flex items-center justify-between">
+                                        <div className="info flex items-center gap-5">
+                                            <Image src={ item.image } alt="image" width={ 80 } className=" rounded-full" />
+                                            <div className="name">
+                                                <p className="text-lg font-semibold capitalize">{ item.name }</p>
+                                                <p className="text-sm text-gray-500 capitalize">{ item.profile }</p>
+                                            </div>
+                                        </div>
+                                        <Image src={ dquote } alt="quote" />
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    })
+                }
+
+            </div>
+        </>
     )
 }
---
+++ src/data/testimonialsData.js
@@ -0,0 +1,19 @@
+import testimonials1 from '../../public/images/testimonials/1.jpg'
+import testimonials2 from '../../public/images/testimonials/2.jpg'
+
+export const testimonialsData = [
+    {
+        id: 1,
+        image: testimonials1,
+        name: 'harry potter',
+        profile: 'banker',
+        review: 'We rented a car from this website and had an amazing experience! The booking was easy and the rental rates were very affordable.'
+    },
+    {
+        id: 2,
+        image: testimonials2,
+        name: 'ron wizzly',
+        profile: 'business owner',
+        review: 'The car was in great condition and made our trip even better. Highly recommend for this car rental website!'
+    },
+]
