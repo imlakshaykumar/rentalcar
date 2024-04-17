ae988572424da95c95d46f637f552c9ca7f339a4
Lakshay kumar <imlakshaykumar@gmail.com>
2024-04-18 00:42:28 +0530

    added bookCar component

---
+++ src/components/BookCar.jsx
@@ -0,0 +1,11 @@
+export default function BookCar() {
+    return (
+        // FIXME: fix this
+        <div className="max-w-[1500px] mx-auto">
+            <div className="border-div">
+                {/* <p>Book a car</p> */ }
+
+            </div>
+        </div>
+    )
+}
